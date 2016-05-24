40f60e973b124355397be289b3d0e609dab630d2
TJB III <thomasbuida@gmail.com>
2016-05-24 15:20:20 -0500

    hidden html field work-around from denise

--- art_gallery/art_gallery/Models/OwnerInventory.cs
+++ art_gallery/art_gallery/Models/OwnerInventory.cs
@@ -7,18 +7,23 @@ namespace art_gallery.Models
 {
     public class OwnerInventory
     {
+        //from artwork
         public int ArtWorkId { get; set; }
         public int ArtistId { get; set; }
         public string Title { get; set; }
         public int NumberInInventory { get; set; }
         public int NumberSold { get; set; }
 
+        //from individual piece
         public int IndividualPieceId { get; set; }
         public string Image { get; set; }
         public decimal Cost { get; set; }
         public decimal Price { get; set; }
         public bool Sold { get; set; }
+        public string Location { get; set; }
+        public string EditionNumber { get; set; }
 
+        //from artist
         public string Name { get; set; }
     }
 }
