c7e740960dc278f3049ea1b80c1014802126f6ec
Jen Solima <jensolima@gmail.com>
2016-05-25 15:03:13 -0500

    added Agents - create, delete & edit

---
+++ art_gallery/art_gallery/ViewModel/AgentDetailViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using art_gallery.Models;
+
+namespace art_gallery.ViewModel
+{
+    public class AgentDetailViewModel
+    {
+        // from agent
+        public int AgentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool Active { get; set; }
+
+        //from invoice
+        public int IndividualPieceId { get; set; }
+
+        //from individualPiece
+        public decimal Cost { get; set; }
+
+        public List<Agent> Agent { get; set; }
+    }
+}
---
+++ art_gallery/art_gallery/ViewModel/AgentListViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using art_gallery.Models;
+
+namespace art_gallery.ViewModel
+{
+    public class AgentListViewModel
+    {
+        // from agent
+        public int AgentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool Active { get; set; }
+
+        ////from invoice
+        //public int IndividualPieceId { get; set; }
+
+        ////from individualPiece
+        //public decimal Cost { get; set; }
+
+        public List<AgentDetailViewModel> Agents { get; set; }
+
+    }
+}
