4439d9d23d0001b20c26c4a1bdf89434dba60189
Martin Cross <mncross@gmail.com>
2016-05-27 11:58:23 -0500

    razor dropdown no bueno. filter price range by link.

--- art_gallery/art_gallery/ViewModel/ArtDetailViewModel.cs
+++ art_gallery/art_gallery/ViewModel/ArtDetailViewModel.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using art_gallery.Models;
+using System.Web.Mvc;
 
 namespace art_gallery.ViewModel
 {
   public class ArtDetailViewModel
   {
     public List<ArtWorkWithImagesViewModel> ArtListings { get; set; }
+    public List<decimal> priceFloor { get; set; }
+    public List<decimal> priceCeiling { get; set; }
+    public decimal[] priceRange { get; set; }
+    public decimal selectedPriceFloor { get; set; }
+    public decimal selectedPriceCeiling { get; set; }
+    public SelectList priceOptions { get; set; }
+
+    public ArtDetailViewModel()
+    {
+      priceFloor = new List<decimal>();
+      priceCeiling = new List<decimal>();
+    }
   }
 }
